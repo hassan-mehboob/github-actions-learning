51bb4879b98cbea719da7e6164e0d39661e66d6f
hassan-mehboob <hassanmehboob691@gmail.com>
2021-12-28 16:26:40 +0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Just learning git hub actions ( CI/CD )</h1>
+    </div>
+  )
+}
+
+export default App
